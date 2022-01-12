65a2b52cd43fda3a9d9b16d17846431add7f57b4
tetsu633 <tetsunishimura633@gmail.com>
2022-01-12 18:55:49 +0900

    set image and css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,16 @@
-import Link from "next/link";
+import Head from "next/head";
+
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+      </Layout>
     </>
   );
 };
